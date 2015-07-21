29fd250ada1d278d1f7f49fdbea36bc6b4a61652
Michal Nicpon <nicpon.michal@gmail.com>
2015-07-21 15:16:38 -0600

    Initial commit basic chat room

---
+++ main.go
@@ -0,0 +1,124 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+type Client struct {
+	conn     net.Conn
+	nickname string
+	room     string
+	ch       chan string
+}
+
+func main() {
+	ln, err := net.Listen("tcp", ":6000")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	msgchan := make(chan string)
+	addchan := make(chan Client)
+	rmchan := make(chan Client)
+
+	go handleMessages(msgchan, addchan, rmchan)
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		go handleConnection(conn, msgchan, addchan, rmchan)
+	}
+}
+
+func (c Client) ReadLinesInto(ch chan<- string) {
+	bufc := bufio.NewReader(c.conn)
+	for {
+		line, err := bufc.ReadString('\n')
+		if err != nil {
+			break
+		}
+		ch <- fmt.Sprintf("%s: %s", c.nickname, line)
+	}
+}
+
+func (c Client) WriteLinesFrom(ch <-chan string) {
+	for msg := range ch {
+		_, err := io.WriteString(c.conn, msg)
+		if err != nil {
+			return
+		}
+	}
+}
+
+func promptNick(c net.Conn, bufc *bufio.Reader) string {
+	io.WriteString(c, "Enter a nickname: ")
+	nick, _, _ := bufc.ReadLine()
+	return string(nick)
+}
+
+func promptRoom(c net.Conn, bufc *bufio.Reader) string {
+	io.WriteString(c, "Enter a room: ")
+	room, _, _ := bufc.ReadLine()
+	return string(room)
+}
+
+func handleConnection(c net.Conn, msgchan chan<- string, addchan chan<- Client, rmchan chan<- Client) {
+	bufc := bufio.NewReader(c)
+	defer c.Close()
+	client := Client{
+		conn:     c,
+		nickname: promptNick(c, bufc),
+		room:     promptRoom(c, bufc),
+		ch:       make(chan string),
+	}
+	if strings.TrimSpace(client.nickname) == "" {
+		io.WriteString(c, "Invalid Username\n")
+		return
+	}
+
+	// Register user
+	addchan <- client
+	defer func() {
+		msgchan <- fmt.Sprintf("User %s left the chat room.\n", client.nickname)
+		log.Printf("Connection from %v closed.\n", c.RemoteAddr())
+		rmchan <- client
+	}()
+	io.WriteString(c, fmt.Sprintf("Welcome, %s!\n\n", client.nickname))
+	msgchan <- fmt.Sprintf("New user %s has joined the chat room.\n", client.nickname)
+
+	// I/O
+	go client.WriteLinesFrom(client.ch)
+	client.ReadLinesInto(msgchan)
+}
+
+func handleMessages(msgchan <-chan string, addchan <-chan Client, rmchan <-chan Client) {
+	clients := make(map[net.Conn]chan<- string)
+
+	for {
+		select {
+		case msg := <-msgchan:
+			log.Printf("New message: %s", msg)
+			for _, ch := range clients {
+				go func(mch chan<- string) { mch <- msg }(ch)
+			}
+		case client := <-addchan:
+			log.Printf("New client: %v\n", client.conn)
+			clients[client.conn] = client.ch
+		case client := <-rmchan:
+			log.Printf("Client disconnects: %v\n", client.conn)
+			delete(clients, client.conn)
+		default:
+		}
+	}
+}
