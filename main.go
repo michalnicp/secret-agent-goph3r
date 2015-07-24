c9bd6fb3d0c93d2956fe7c22a050df85699ef4d0
Michal Nicpon <nicpon.michal@gmail.com>
2015-07-24 16:37:37 -0600

    Add glenda and look

--- main.go
+++ main.go
@@ -11,22 +11,22 @@ import (
 )
 
 type Client struct {
-	conn     net.Conn
-	nickname string
-	ch       chan Message
+	Conn     net.Conn
+	Nickname string
+	Ch       chan Message
 }
 
 type Message struct {
-	from Client
-	text string
+	From Client
+	Text string
 }
 
 type Room struct {
-	name    string
-	clients map[net.Conn]*Client
-	addchan chan *Client
-	rmchan  chan Client
-	msgchan chan Message
+	Name    string
+	Clients map[net.Conn]*Client
+	Addchan chan *Client
+	Rmchan  chan Client
+	Msgchan chan Message
 }
 
 func main() {
@@ -39,26 +39,26 @@ func main() {
 	rooms := make(map[string]*Room)
 
 	for {
-		conn, err := ln.Accept()
+		Conn, err := ln.Accept()
 		if err != nil {
 			fmt.Println(err)
 			continue
 		}
 
-		go handleConnection(conn, rooms)
+		go handleConnection(Conn, rooms)
 	}
 }
 
 func (c Client) ReadLinesInto(ch chan<- Message) {
-	bufc := bufio.NewReader(c.conn)
+	bufc := bufio.NewReader(c.Conn)
 	for {
 		line, err := bufc.ReadString('\n')
 		if err != nil {
 			break
 		}
 		message := Message{
-			from: c,
-			text: line,
+			From: c,
+			Text: line,
 		}
 		ch <- message
 	}
@@ -66,8 +66,8 @@ func (c Client) ReadLinesInto(ch chan<- Message) {
 
 func (c Client) WriteLinesFrom(ch <-chan Message) {
 	for msg := range ch {
-		if msg.from != c {
-			_, err := io.WriteString(c.conn, msg.text)
+		if msg.From != c {
+			_, err := io.WriteString(c.Conn, msg.Text)
 			if err != nil {
 				return
 			}
@@ -76,13 +76,13 @@ func (c Client) WriteLinesFrom(ch <-chan Message) {
 }
 
 func promptNick(c net.Conn, bufc *bufio.Reader) string {
-	io.WriteString(c, "Enter a nickname: ")
+	io.WriteString(c, "Enter a Nickname: ")
 	nick, _, _ := bufc.ReadLine()
 	return string(nick)
 }
 
 func promptRoom(c net.Conn, bufc *bufio.Reader) string {
-	io.WriteString(c, "Log in to your team's assigned collaboration channel")
+	io.WriteString(c, "Log in to your team's assigned collaboration channel: ")
 	room, _, _ := bufc.ReadLine()
 	return string(room)
 }
@@ -91,16 +91,16 @@ func handleConnection(c net.Conn, rooms map[string]*Room) {
 	bufc := bufio.NewReader(c)
 	defer c.Close()
 	client := Client{
-		conn:     c,
-		nickname: promptNick(c, bufc),
-		ch:       make(chan Message),
+		Conn:     c,
+		Nickname: promptNick(c, bufc),
+		Ch:       make(chan Message),
 	}
-	if strings.TrimSpace(client.nickname) == "" {
+	if strings.TrimSpace(client.Nickname) == "" {
 		io.WriteString(c, "Invalid Username\n")
 		return
 	}
-	welcomeMsg := `A monolithic building appears before you. You have arrived
-				   at the office. Try not to act suspicious.`
+	welcomeMsg := "A monolithic building appears before you. You have arrived\n" +
+		"at the office. Try not to act suspicious.\n\n"
 	io.WriteString(c, welcomeMsg)
 
 	roomName := promptRoom(c, bufc)
@@ -108,75 +108,100 @@ func handleConnection(c net.Conn, rooms map[string]*Room) {
 	if !ok {
 		// Create a new room with name
 		room = &Room{
-			name:    roomName,
-			clients: make(map[net.Conn]*Client, 0),
-			addchan: make(chan *Client),
-			rmchan:  make(chan Client),
-			msgchan: make(chan Message),
+			Name:    roomName,
+			Clients: make(map[net.Conn]*Client, 0),
+			Addchan: make(chan *Client),
+			Rmchan:  make(chan Client),
+			Msgchan: make(chan Message),
 		}
 		rooms[roomName] = room
 		go handleIO(rooms[roomName])
 	}
 
-	if len(room.clients) < 3 {
-		// Register user
-		room.addchan <- &client
-		defer func() {
-			room.msgchan <- Message{
-				text: fmt.Sprintf("%s has left %s.\n", client.nickname, room.name),
-			}
-			log.Printf("Connection from %v closed.\n", c.RemoteAddr())
-			room.rmchan <- client
-		}()
-		room.msgchan <- Message{
-			text: fmt.Sprintf("--> | %s has joined %s, waiting for teammates...\n", client.nickname, room.name),
+	if len(room.Clients) < 3 {
+		room.Addchan <- &client
+		room.Msgchan <- Message{
+			Text: fmt.Sprintf("--> | %s has joined %s, waiting for teammates...\n", client.Nickname, room.Name),
 		}
 
+		defer func() {
+			room.Msgchan <- Message{
+				Text: fmt.Sprintf("--> | %s has left %s.\n", client.Nickname, room.Name),
+			}
+			log.Printf("Connection From %v closed", c.RemoteAddr())
+			room.Rmchan <- client
+		}()
+
+		go client.WriteLinesFrom(client.Ch)
+		if len(room.Clients) == 3 {
+			startMsg := "* -- | Everyone has arrived, mission starting...\n" +
+				"* -- | Ask for /help to get familiar around here\n"
+			room.Msgchan <- Message{Text: startMsg}
+			// Glenda always shows up last
+			conn, err := net.Dial("tcp", "127.0.0.1:6000")
+			if err != nil {
+				log.Printf("Glenda could not connect")
+			}
+			glenda := Client{
+				Conn:     conn,
+				Nickname: "Glenda",
+				Ch:       make(chan Message),
+			}
+			room.Addchan <- &glenda
+		}
+		client.ReadLinesInto(room.Msgchan)
 	} else {
-		fullMsg := `I'm sorry, it seems your teammates have started without
-		            you. Please try again`
-		io.WriteString(c, fullMsg)
-	}
-	// if everyone is present, start accepting I/O
-	if len(room.clients) == 3 {
-		startMsg := `*-- | Everyone has arrived, mission starting...
-		             Ask for /help to get familiar around here`
-		io.WriteString(c, startMsg)
-		go client.WriteLinesFrom(client.ch)
-		client.ReadLinesInto(room.msgchan)
+		lateMsg := "It seems your teammates have started without you. Find better friends\n"
+		io.WriteString(c, lateMsg)
 	}
 }
 
 func handleIO(r *Room) {
-	// handle all io from clients
+	// handle all io From Clients
 	for {
 		select {
-		case msg := <-r.msgchan:
-			log.Printf("%s: %s", msg.from.nickname, msg.text)
-			for _, client := range r.clients {
-				client.ch <- msg
+		case msg := <-r.Msgchan:
+			msgText := strings.Trim(strings.Replace(msg.Text, "\n", "", -1), " ")
+			first := strings.Split(msgText, " ")[0]
+			switch first {
+			case "/help":
+				help(msg.From.Ch)
+			case "/look":
+				look(msg.From.Ch, r.Clients)
+			default:
+				for _, client := range r.Clients {
+					client.Ch <- msg
+				}
 			}
-		case client := <-r.addchan:
-			log.Printf("New client: %v\n", client.conn)
-			r.clients[client.conn] = client
-		case client := <-r.rmchan:
-			log.Printf("Client disconnects: %v\n", client.conn)
-			delete(r.clients, client.conn)
+		case client := <-r.Addchan:
+			log.Printf("New client: %v", client.Conn)
+			r.Clients[client.Conn] = client
+		case client := <-r.Rmchan:
+			log.Printf("Client disconnects: %v", client.Conn)
+			delete(r.Clients, client.Conn)
 		default:
 		}
 	}
 }
 
-func printHelp(ch chan<- Message) {
-	msg := `help -- |  Usage:
-            help -- |
-            help -- |     /[cmd] [arguments]
-            help -- |
-            help -- |  Available commands:
-            help -- |
-            help -- |    /msg [to] [text]         send message to coworker
-            help -- |    /list                    look at files you have access to
-            help -- |    /send [to] [filename]    move file to coworker
-            help -- |    /look                    show coworkers`
-	ch <- Message{text: msg}
+func help(ch chan Message) {
+	helpText := "help -- |  Usage:\n" +
+		"help -- |\n" +
+		"help -- |     /[cmd] [arguments]\n" +
+		"help -- |\n" +
+		"help -- |  Available commands:\n" +
+		"help -- |\n" +
+		"help -- |    /msg [to] [text]         send message to coworker\n" +
+		"help -- |    /list                    look at files you have access to\n" +
+		"help -- |    /send [to] [filename]    move file to coworker\n" +
+		"help -- |    /look                    show coworkers\n"
+	ch <- Message{Text: helpText}
+}
+
+func look(ch chan Message, clients map[net.Conn]*Client) {
+	lookText := "look -- | You look around at your co-workers' nametages:\n"
+	for _, c := range clients {
+		lookText += ("look -- | " + c.Nickname + "\n")
+	}
+	ch <- Message{Text: lookText}
 }
